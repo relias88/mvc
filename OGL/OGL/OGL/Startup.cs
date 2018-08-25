ae685ad29126fd1587afae0ddc20cfb4c4a65f79
Eli <rafal.elias@outlook.com>
2018-08-26 00:58:35 +0200

    init commit

---
+++ OGL/OGL/OGL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OGL.Startup))]
+namespace OGL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
