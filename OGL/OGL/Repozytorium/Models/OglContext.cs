2226e95788ea3cda9e7bd75a2b3c7e0530e47929
Eli <rafal.elias@outlook.com>
2018-09-01 01:20:13 +0200

    Project after migration effect - add problemHelper.txt

---
+++ OGL/OGL/Repozytorium/Models/OglContext.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Repozytorium.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    
+
+    public class OglContext : IdentityDbContext
+    {
+        public OglContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static OglContext Create()
+        {
+            return new OglContext();
+        }
+
+        public DbSet<Kategoria> Kategorie { get; set; }
+        public DbSet<Ogloszenie> Ogloszenia { get; set; }
+        public DbSet<Uzytkownik> Uzytkownik { get; set; }
+        public DbSet<Ogloszenie_Kategoria> Ogloszenie_Kategoria { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            modelBuilder.Entity<Ogloszenie>()
+                .HasRequired(u => u.Uzytkownik)
+                .WithMany(x => x.Ogloszenia)
+                .HasForeignKey(x => x.UzytkownikId)
+                .WillCascadeOnDelete(true);
+        }
+    }
+}
---
+++ OGL/OGL/Repozytorium/Models/Ogloszenie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Repozytorium.Models
+{
+    public class Ogloszenie
+    {
+        public Ogloszenie()
+        {
+            this.Ogloszenie_Kategoria = new HashSet<Ogloszenie_Kategoria>();
+        }
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Tresc ogłoszenia:")]
+        [MaxLength(72)]
+        public string Tresc { get; set; }
+
+        [Display(Name = "Tytuł ogłoszenia:")]
+        [MaxLength(72)]
+        public string Tytul { get; set; }
+
+        [Display(Name = "Data dodania:")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DataDodania { get; set; }
+
+        public virtual ICollection<Ogloszenie_Kategoria> Ogloszenie_Kategoria { get; set; }
+
+        public string UzytkownikId { get; set; }
+
+        public virtual Uzytkownik Uzytkownik { get; set; }
+    }
+}
