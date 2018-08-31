b21292c27f520c52710436ce11d74b420dcc2f49
Eli <rafal.elias@outlook.com>
2018-09-01 01:15:05 +0200

    Project after migration effect - add problemHelper.txt

--- OGL/OGL/OGL/App_Start/IdentityConfig.cs
+++ OGL/OGL/OGL/App_Start/IdentityConfig.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using OGL.Models;
+using Repozytorium.Models;
 
 namespace OGL
 {
